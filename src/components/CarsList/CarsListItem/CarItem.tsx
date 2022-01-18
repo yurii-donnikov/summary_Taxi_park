acaf623b91a2dad5ac53c40153d987fc27da4490
Stanislav-Karasyk <99.kara.99@gmail.com>
2022-01-18 18:55:31 +0200

    fix listItem

--- src/components/CarsList/CarsListItem/CarItem.tsx
+++ src/components/CarsList/CarsListItem/CarItem.tsx
@@ -6,26 +6,30 @@ import { getDrivers } from '../../../redux/drivers/driversSelectors';
 import {
   fetchCarsRequest,
   fetchCarsByIdDriverRequest,
+  deleteCarRequest,
 } from '../../../redux/cars/carsActions';
 import { fetchDriversRequest } from '../../../redux/drivers/driversActions';
-import CarsTable from './CarsTable';
+// import CarsTable from './CarsTable';
 import { useParams } from 'react-router-dom';
 
+import sprite from '../../../icons/symbol-defs.svg';
 
 const CarItem = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
   let cars = useSelector(getCars);
-  
- 
+
+  const handlerDeleteBtn = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const carId = Number(event.currentTarget.id);
+    dispatch(deleteCarRequest(carId));
+  };
 
   useEffect(() => {
     if (id) {
       dispatch(fetchCarsByIdDriverRequest(Number(id)));
-    }else {
+    } else {
       dispatch(fetchCarsRequest());
     }
-    
   }, [dispatch, id]);
 
   const drivers = useSelector(getDrivers);
@@ -35,29 +39,30 @@ const CarItem = () => {
   }, [dispatch]);
   return (
     <>
-      {cars.map(car => (
-        <li key={car.id} className={styles.data_list}>
-          {drivers
-            .filter(driver => driver.id === car.driver_id)
-            .map(driver => (
-              <CarsTable
-                driverName={driver.first_name}
-                driverId={driver.id}
-                carId={car.id}
-                driverLastName={driver.last_name}
-                driverStatus={driver.status.title}
-                carModel={car.model}
-                carMark={car.mark}
-                carYear={car.year}
-                carNumber={car.number}
-                carStatus={car.status.title}
-                key={driver.id}
-              />
-            ))}
-        </li>
-      ))}
+      {cars.map(car =>
+        drivers
+          .filter(driver => driver.id === car.driver_id)
+          .map(driver => (
+            <li key={car.id} className={styles.data_list}>
+              <p>{car.id}</p>
+              <p title={driver.status.title} key={driver.id}>
+                {driver.first_name + ' ' + driver.last_name}
+                <p>{car.model}</p>
+                <p>{car.mark}</p>
+                <p>{car.year}</p>
+                <p>{car.number}</p>
+                <p>{car.status.title}</p>
+                <button id={car.id.toString()} onClick={handlerDeleteBtn}>
+                  <svg className={styles.icon}>
+                    {' '}
+                    <use href={sprite + '#icon-TypeDendie'} />{' '}
+                  </svg>
+                </button>
+              </p>
+            </li>
+          )),
+      )}
     </>
   );
 };
-
 export default CarItem;
